5947bc2122fb49e48db3124977dbb4da8cc92a06
Lulakshi Hettiarachchi <lulakshinadunihettiarachchi@gmail.com>
2022-10-27 16:00:39 +0530

    first commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Col } from 'react-bootstrap'
+
+
+const Footer = () => {
+      return (
+            <footer>
+                  <Container>
+                        <Col className='text-center py-3'>
+                              &copy; Created by Madusanka | Agasthi | Ushini | Bulner
+                        </Col>
+                  </Container>
+            </footer>
+      )
+}
+
+export default Footer
